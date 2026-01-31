248f8e8f19bcef1ac49c4b7d35d4100b8db65344
christinaotoboh <otobohsmiles@gmail.com>
2026-01-31 14:32:23 +0100

    chore: update backend config with new infrastructure values

--- scripts/configure-backend-instance.sh
+++ scripts/configure-backend-instance.sh
@@ -12,14 +12,14 @@ echo "Configuring instance: $INSTANCE_ID"
 
 # Create .env file content
 ENV_CONTENT='PORT=8080
-MONGO_URI=mongodb://admin:StartTech2024SecurePassword!@10.0.2.109:27017
+MONGO_URI=mongodb://admin:StartTech2024SecurePassword!@10.0.2.19:27017
 DB_NAME=much_todo_db
 ENABLE_CACHE=true
 REDIS_ADDR=starttech-redis.jlvbpx.0001.use1.cache.amazonaws.com:6379
 JWT_SECRET_KEY=StartTech2024SuperSecureJWTKey!RandomString123456789
 JWT_EXPIRATION_HOURS=72
-ALLOWED_ORIGINS=https://d3b7s0k4cs6xoj.cloudfront.net
-COOKIE_DOMAINS=d3b7s0k4cs6xoj.cloudfront.net
+ALLOWED_ORIGINS=https://d2suwtb3fkg2xw.cloudfront.net
+COOKIE_DOMAINS=d2suwtb3fkg2xw.cloudfront.net
 SECURE_COOKIE=false
 LOG_LEVEL=INFO
 LOG_FORMAT=json'
